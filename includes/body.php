d0041fdf168cc47add008dcb77ea801eeb2f267a
Chinaechetam Maduabuchi <esokawumaduabuchic@gmail.com>
2017-10-10 15:22:22 +0100

    first commit

---
+++ includes/body.php
@@ -0,0 +1,59 @@
+<body class="container-fluid">
+    <section class="row">
+      <div class="col-sm-12">
+    
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+               <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span> 
+                  </button>
+               <a class="navbar-brand" href="index.php"><img src="/lnq/images/lnq-logo.jpeg" alt="lnq logo" height="100" width="120"/></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                   <li><a class="custom-a" href="#">SIGN UP&nbsp<span class="glyphicon glyphicon-new-window"></span></a></li>
+                   <li><a class="custom-a" href="#">SIGN IN&nbsp<span class="glyphicon glyphicon-log-in"></span></a></li> 
+                   <li><a class="custom-a" href="#">ABOUT&nbsp<span class="glyphicon glyphicon-info-sign"></span></a></li>
+                   <li><a class="custom-a" href="#">PRIVACY POLICY&nbsp<span class="glyphicon glyphicon-lock"></span></a></li>
+                   <li><a class="custom-a" href="#">CONTACT&nbsp<span class="glyphicon glyphicon-envelope"></span></a></li> 
+                </ul>
+            </div>
+ 
+           </div>
+        </nav>
+
+      </div>
+
+
+    
+      <div class="col-sm-12 main-section">
+
+        <h1 id="mainHead">BEYOND CHATTING</h1>
+        
+        <p id="mainP">A NEW WAY TO CONNECT THOSE THAT MATTER AND MEET NEW PEOPLE...</p>
+
+        <a href="#" class="btn btn-primary" id="getStarted">GET STARTED &nbsp;<span class="glyphicon glyphicon-plus-sign"><span></a><br><br><br><br><br><br><br><br>
+        
+      </div>
+
+      <div class="col-sm-12 main-section2">
+          
+            <h2 id="subHead">CONNECT THE WOLRD</h2>
+
+            <p id="subP">
+
+                On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, <br>
+                so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue.
+
+            </p><br><br><br>
+
+            <footer class="row" style="height:50px; background:url('images/p.jpg'); border-top:solid 1px #0c754a; font-family:Trebuchet MS; color:#0C754A; text-align:center;">
+                <span class="col-sm-12" style="padding:15px 15px;">  &copy; <?php echo date("Y")?> lnQ-All Rights Reserved.</span>
+            </footer>
+
+      </div>
+
+    </section>
---
+++ includes/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>lnq</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/lnq/css/bootstrap.min.css">
+        <script src="/lnq/js/jquery-3.2.1.min.js"></script>
+        <script src="/lnq/js/bootstrap.min.js"></script>
+        <link href="/lnq/css/custom.css" rel="stylesheet">
+    </head>
+
+   
